ddbe77f59409c64e1afda5319a36a528004a9247
Sag is the best <87934102+msajo@users.noreply.github.com>
2023-04-03 17:35:45 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 Harry_potter = "";
 petar_pan = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload(){
     Harry_potter.loadSound("music.mp3");
     petar_pan.loadSound("music2.mp3");
@@ -9,7 +13,22 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("Left Wrist X position is "+ leftWristX + " Left Wrist Y position is "+ leftWristY);
+        console.log("Right Wrist X position is "+ rightWristX + " Right Wrist Y position is " + rightWristY);   
+    }
 }
